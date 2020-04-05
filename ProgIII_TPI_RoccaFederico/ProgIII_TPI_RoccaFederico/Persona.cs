4edfad00fc6b4a0094ff81c9175707afffea9e2a
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-04-05 18:16:50 -0300

    Agregado de clases:     Persona     Sede     Alumno     Profesor     Directivo     Localizacion     Carrera     Evaluacion     Materia     Contacto     Inscripcion     Cursada

---
+++ ProgIII_TPI_RoccaFederico/ProgIII_TPI_RoccaFederico/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgIII_TPI_RoccaFederico
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public int dni { get; set; }
+    }
+}
---
+++ ProgIII_TPI_RoccaFederico/ProgIII_TPI_RoccaFederico/Sede.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgIII_TPI_RoccaFederico
+{
+    class Sede
+    {
+    }
+}
